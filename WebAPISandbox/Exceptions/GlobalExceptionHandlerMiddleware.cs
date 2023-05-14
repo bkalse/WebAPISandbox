d1f2ab9da5e848e53947f3ee212337dc50bac7a2
bkalse <bhushan_kalse@hotmail.com>
2023-05-14 18:39:07 +1000

    Add project files.

---
+++ WebAPISandbox/Exceptions/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+
+namespace WebAPISandbox.Exceptions
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;   
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode status = HttpStatusCode.InternalServerError;
+            var stackTrace = string.Empty;
+            var message = string.Empty;
+
+            var exceptionType = exception.GetType();
+
+            if (exceptionType == typeof(BadRequestException))
+            {
+                message = exception.Message;
+                status = HttpStatusCode.BadRequest;
+                stackTrace = exception.StackTrace;
+            }
+            else if (exceptionType == typeof(NotFoundException))
+            {
+                message = exception.Message;
+                status = HttpStatusCode.NotFound;
+                stackTrace = exception.StackTrace;
+            }
+
+            var exceptionResult = JsonSerializer.Serialize(new { error = message});
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)status;
+
+            //return context.Response.WriteAsync(exceptionResult);
+            await context.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error from the custom middleware."
+            }.ToString());
+        }
+    }
+}
